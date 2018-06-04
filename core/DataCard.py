8f479f2a71ee057682b4378f78e325c847293c75
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-06-04 20:18:03 +0100

    Improve datacard generation

--- core/DataCard.py
+++ core/DataCard.py
@@ -8,7 +8,6 @@ Systematic = namedtuple('Systematic', ['value'])
 class DataCard(object):
     def __init__(self, filename):
         self.filename = filename
-        self.f = open(filename, 'w')
         self.regions = odict()
         self.systematics = odict()
         self.rate_params = []
@@ -35,79 +34,127 @@ class DataCard(object):
             additional_args = ""
 
         self.rate_params.append(
-            " ".join([param, "rateParam", region, process, str(value), additional_args])
+            [param, "rateParam", region, process, str(value), additional_args]
         )
 
-    def write_break(self):
-        self.f.write("------------\n")
+    def sort_systematics(self, sorted_list):
+        if set(sorted(self.systematics.keys())) != set(sorted(sorted_list)):
+            missing = list(set(sorted(self.systematics.keys())) - set(sorted(sorted_list)))
+            print "Trying to sort systematics, but missing: {}\nWill skip sorting".format(missing)
+            return
+        self.systematics = odict([(k, self.systematics[k]) for k in sorted_list])
 
-    def write_header(self):
-        self.f.write("imax *  number of channels\n")
-        self.f.write("jmax *  number of backgrounds\n")
-        self.f.write("kmax *  number of nuisance parameters\n")
+    def textblock_break(self):
+        return self.format_lines([['------------']])
 
-    def write_bins(self):
-        line = "bin         "
-        line += " ".join(self.regions.keys())
-        self.f.write(line+"\n")
+    def textblock_header(self):
+        lines = [
+            ["imax", "*", "number of channels"],
+            ["jmax", "*", "number of backgrounds"],
+            ["kmax", "*", "number of nuisance parameters"],
+        ]
+        return self.format_lines(lines)
 
-        line = "observation "
-        line += " ".join([reg.obs for regionname, reg in self.regions.items()])
-        self.f.write(line+"\n")
+    def textblock_bins(self):
+        """
+        e.g.
+        [
+            ['bin', 'monojet', 'singlemu', 'doublemu'],
+            ['observation', '155933.0', '63095.0', '7286.0'],
+        ]
+        """
+        lines = [
+            ["bin"] + self.regions.keys(),
+            ["observation"] + [reg.obs for reg in self.regions.values()],
+        ]
+        return self.format_lines(lines)
 
-    def write_procs(self):
-        line = "bin         "
-        line += " ".join([" ".join([regname]*len(reg.processes)) for regname, reg in self.regions.items()])
-        self.f.write(line+"\n")
+    def textblock_procs(self):
+        """
+        e.g.
+        [
+            ['bin', 'monojet', 'monojet', 'monojet', 'monojet', 'singlemu', 'singlemu', 'singlemu', 'doublemu', 'doublemu', 'doublemu'],
+            ['process', 'znunu', 'wlnu', 'qcd', 'bkg', 'wlnu', 'qcd', 'bkg', 'dymumu', 'qcd', 'bkg'],
+            ['process', '0', '1', '2', '3', '1', '2', '3', '0', '1', '2'],
+            ['rate', '91060.8', '53907.3', '298.7', '4108.7', '61873.6', '641.6', '4117.1', '7341.8', '1e-10', '204.2'],
+        ]
+        """
+        lines = [
+            ["bin"] + [entry
+                       for regname, reg in self.regions.items()
+                       for entry in [regname]*len(reg.processes)],
+            ["process"] + [procname
+                           for reg in self.regions.values()
+                           for procname in reg.processes.keys()],
+            ["process"] + [str(proc.idx)
+                           for reg in self.regions.values()
+                           for proc in reg.processes.values()],
+            ["rate"] + [proc.rate
+                        for reg in self.regions.values()
+                        for proc in reg.processes.values()],
+        ]
+        return self.format_lines(lines)
 
-        line = "process     "
-        line += " ".join([procname
-            for reg in self.regions.values()
-            for procname in reg.processes.keys()
+    def textblock_systs(self):
+        """
+        create lines of columns, e.g.:
+        [
+            ['CMS_lumi', 'lnN', '1.025', '-', '-', '1.025', '-', '-', '1.025', '1.025', '-', '1.025'],
+            ...
+        ]
+        where the values are for the respective (region, process). '-' are for
+        (region, process) where the systematic isn't applied
+        """
+        lines = [
+            [systname, pdf] + [
+                str(proc.systematics[systname].value) \
+                    if systname in proc.systematics \
+                    else '-'
+                for regname, reg in self.regions.items()
+                for procname, proc in reg.processes.items()
+            ] for systname, pdf in self.systematics.items()
+        ]
+        return self.format_lines(lines)
+
+    def textblock_rate_params(self):
+        """
+        e.g.
+        [
+            ['tf_wlnu', 'rateParam', 'monojet', 'wlnu', '1'],
+            ['tf_wlnu', 'rateParam', 'singlemu', 'wlnu', '1'],
+            ['r_mumu', 'rateParam', 'doublemu', 'dymumu', '1'],
+            ['r_nunu', 'rateParam', 'monojet', 'znunu', '(@0*@1) r,r_mumu'],
+        ]
+        """
+        lines = [rate_param for rate_param in self.rate_params]
+        return self.format_lines(lines)
+
+    def format_lines(self, lines):
+        row_maxes = [max([len(entry) for entry in rows]) for rows in zip(*lines)]
+        return "\n".join([
+            " ".join([entry+" "*(row_max-len(entry))
+                for row_max, entry in zip(row_maxes, columns)
+            ]).rstrip() for columns in lines
         ])
-        self.f.write(line+"\n")
-
-        line = "process     "
-        line += " ".join([str(proc.idx)
-            for reg in self.regions.values()
-            for proc in reg.processes.values()
-        ])
-        self.f.write(line+"\n")
-
-        line = "rate        "
-        line += " ".join([proc.rate
-            for reg in self.regions.values()
-            for proc in reg.processes.values()
-        ])
-        self.f.write(line+"\n")
-
-    def write_systs(self):
-        for systname, pdf in self.systematics.items():
-            line = "{} {} ".format(systname, pdf)
-            to_add = []
-            for regname, reg in self.regions.items():
-                for procname, proc in reg.processes.items():
-                    if systname in proc.systematics:
-                        value = str(proc.systematics[systname].value)
-                    else:
-                        value = '-'
-                    to_add.append(value)
-            line += " ".join(to_add)
-            self.f.write(line+"\n")
-
-    def write_rate_params(self):
-        for rate_param in self.rate_params:
-            self.f.write(rate_param+"\n")
 
     def write(self):
-        self.write_header()
-        self.write_break()
-        self.write_bins()
-        self.write_break()
-        self.write_procs()
-        self.write_break()
-        self.write_systs()
-        self.write_break()
-        self.write_rate_params()
-        self.f.close()
+        lines = "\n".join([
+            self.textblock_header(),
+            self.textblock_break(),
+            self.textblock_bins(),
+            self.textblock_break(),
+            self.textblock_procs(),
+            self.textblock_break(),
+            self.textblock_systs(),
+            self.textblock_break(),
+            self.textblock_rate_params(),
+        ])
+        with open(self.filename, 'w') as f:
+            f.write(lines)
         print "Created datacard {}".format(self.filename)
+
+    def clear(self):
+        self.filename = None
+        self.regions = odict()
+        self.systematics = odict()
+        self.rate_params = []
--- scripts/generate_datacards.py
+++ scripts/generate_datacards.py
@@ -20,7 +20,7 @@ def parse_args():
 def setup_regions(config_path):
     Inputs = namedtuple('Inputs', ['regions'])
     Region = namedtuple('Region', ['processes'])
-    Process = namedtuple('Process', ['content', 'systs'])
+    Process = namedtuple('Process', ['content', 'stats', 'systs'])
     Systematic = namedtuple('Systematic', ['up', 'down'])
 
     indir = os.path.dirname(config_path)
@@ -31,11 +31,17 @@ def setup_regions(config_path):
             hist.set_directory(None)
         return hist
 
-    def hist_to_value(hist):
-        return hist.integral(overflow=True)
+    def hist_to_value(hist, stats=False):
+        if stats:
+            return hist.integral(overflow=True, error=True)[1]
+        else:
+            return hist.integral(overflow=True)
 
     def get(*args):
         return hist_to_value(get_hist(*args))
+
+    def get_stats(*args):
+        return hist_to_value(get_hist(*args), stats=True)
 
     with open(config_path, 'r') as f:
         input_config = yaml.load(f)
@@ -64,6 +70,7 @@ def setup_regions(config_path):
 
             processes[process] = Process(
                 content = get(central_path),
+                stats = get_stats(central_path),
                 systs = systs,
             )
 
@@ -76,15 +83,23 @@ if __name__ == "__main__":
     outdir = options.outdir
 
     # Monojet
-    dc = DataCard(os.path.join(outdir, "monojet.txt"))
+    dc = DataCard(os.path.join(outdir, "zinv.txt"))
     dc.add_region("monojet", inputs.regions["monojet"].processes["data"].content)
-    for idx, proc in enumerate(["znunu", "wlnu", "bkg"]):
+    for idx, proc in enumerate(["znunu", "wlnu", "qcd", "bkg"]):
         nominal = inputs.regions["monojet"].processes[proc].content
+        stats = inputs.regions["monojet"].processes[proc].stats
+        neff = int((stats / nominal)**2)+1 if nominal>0. else 0
+        alpha = nominal / neff if neff>0 else 0.
         dc.add_process("monojet", proc, idx, nominal)
 
         # lumi
-        if proc != "wlnu":
+        if proc in ["znunu", "bkg"]:
             dc.add_systematic("monojet", proc, "CMS_lumi", "lnN", 1.025)
+        if proc in ["qcd"]:
+            dc.add_systematic("monojet", proc, "CMS_qcd", "lnN", 1.20)
+            #dc.add_systematic("monojet", proc, "CMS_mcstat_qcd", "gmN {}".format(neff), alpha)
+            if nominal == 0.:
+                continue
 
         # PU
         pu_up = inputs.regions["monojet"].processes[proc].systs["pu"].up / nominal
@@ -94,57 +109,76 @@ if __name__ == "__main__":
         # MET trigger
         met_up = inputs.regions["monojet"].processes[proc].systs["metTrig0muSF"].up / nominal
         met_down = inputs.regions["monojet"].processes[proc].systs["metTrig0muSF"].down / nominal
-        dc.add_systematic("monojet", proc, "CMS_metTrig0muSF", "lnN", 1+(abs(met_up-1)+abs(1-met_down))/2)
+        dc.add_systematic("monojet", proc, "CMS_metTrigSF", "lnN", 1+(abs(met_up-1)+abs(1-met_down))/2)
 
         # JECs
         jec_up = inputs.regions["monojet"].processes[proc].systs["jec"].up / nominal
         jec_down = inputs.regions["monojet"].processes[proc].systs["jec"].down / nominal
         dc.add_systematic("monojet", proc, "CMS_jec", "lnN", 1+(abs(jec_up-1)+abs(1-jec_down))/2)
     dc.add_rateparam("tf_wlnu", "monojet", "wlnu", 1)
-    dc.write()
 
     # Single Muon
-    dc = DataCard(os.path.join(outdir, "singlemu.txt"))
     dc.add_region("singlemu", inputs.regions["singlemu"].processes["data"].content)
-    for idx, proc in odict([(1, "wlnu"), (2, "bkg")]).items():
+    for idx, proc in odict([(1, "wlnu"), (2, "qcd"), (3, "bkg")]).items():
         nominal = inputs.regions["singlemu"].processes[proc].content
+        stats = inputs.regions["singlemu"].processes[proc].stats
+        neff = int((stats / nominal)**2)+1 if nominal>0. else 0
+        alpha = nominal / neff if neff>0 else 0.
         dc.add_process("singlemu", proc, idx, nominal)
 
         # lumi
-        if proc != "wlnu":
+        if proc in ["bkg"]:
             dc.add_systematic("singlemu", proc, "CMS_lumi", "lnN", 1.025)
-
+        if proc in ["qcd"]:
+            dc.add_systematic("singlemu", proc, "CMS_qcd", "lnN", 1.20)
+            if nominal == 0.:
+                continue
         # PU
         pu_up = inputs.regions["singlemu"].processes[proc].systs["pu"].up / nominal
         pu_down = inputs.regions["singlemu"].processes[proc].systs["pu"].down / nominal
         dc.add_systematic("singlemu", proc, "CMS_puReweight", "lnN", 1+(abs(pu_up-1)+abs(1-pu_down))/2)
 
         # Muon SF
-        muon_up = inputs.regions["singlemu"].processes[proc].systs["muonSF"].up / nominal
-        muon_down = inputs.regions["singlemu"].processes[proc].systs["muonSF"].down / nominal
-        dc.add_systematic("singlemu", proc, "CMS_muonSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
+        muon_up = inputs.regions["singlemu"].processes[proc].systs["muonIsoSF"].up / nominal
+        muon_down = inputs.regions["singlemu"].processes[proc].systs["muonIsoSF"].down / nominal
+        dc.add_systematic("singlemu", proc, "CMS_muonIsoSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
+
+        muon_up = inputs.regions["singlemu"].processes[proc].systs["muonIdSF"].up / nominal
+        muon_down = inputs.regions["singlemu"].processes[proc].systs["muonIdSF"].down / nominal
+        dc.add_systematic("singlemu", proc, "CMS_muonIdSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
+
+        muon_up = inputs.regions["singlemu"].processes[proc].systs["muonTrackSF"].up / nominal
+        muon_down = inputs.regions["singlemu"].processes[proc].systs["muonTrackSF"].down / nominal
+        dc.add_systematic("singlemu", proc, "CMS_muonTrackSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
 
         # MET trigger
         met_up = inputs.regions["singlemu"].processes[proc].systs["metTrig1muSF"].up / nominal
         met_down = inputs.regions["singlemu"].processes[proc].systs["metTrig1muSF"].down / nominal
-        dc.add_systematic("singlemu", proc, "CMS_metTrig1muSF", "lnN", 1+(abs(met_up-1)+abs(1-met_down))/2)
+        dc.add_systematic("singlemu", proc, "CMS_metTrigMuSF", "lnN", 1+(abs(met_up-1)+abs(1-met_down))/2)
 
         # JECs
         jec_up = inputs.regions["singlemu"].processes[proc].systs["jec"].up / nominal
         jec_down = inputs.regions["singlemu"].processes[proc].systs["jec"].down / nominal
         dc.add_systematic("singlemu", proc, "CMS_jec", "lnN", 1+(abs(jec_up-1)+abs(1-jec_down))/2)
     dc.add_rateparam("tf_wlnu", "singlemu", "wlnu", 1)
-    dc.write()
 
     # Double Muon
-    dc = DataCard(os.path.join(outdir, "doublemu.txt"))
     dc.add_region("doublemu", inputs.regions["doublemu"].processes["data"].content)
-    for idx, proc in odict([(0, "dymumu"), (1, "bkg")]).items():
+    for idx, proc in odict([(0, "dymumu"), (1, "qcd"), (2, "bkg")]).items():
         nominal = inputs.regions["doublemu"].processes[proc].content
+        stats = inputs.regions["doublemu"].processes[proc].stats
+        neff = int((stats / nominal)**2)+1 if nominal>0. else 0.
+        alpha = nominal / neff if neff>0 else 0.
         dc.add_process("doublemu", proc, idx, nominal)
 
         # lumi
-        dc.add_systematic("doublemu", proc, "CMS_lumi", "lnN", 1.025)
+        if proc in ["dymumu", "bkg"]:
+            dc.add_systematic("doublemu", proc, "CMS_lumi", "lnN", 1.025)
+        if proc in ["qcd"]:
+            dc.add_systematic("doublemu", proc, "CMS_qcd", "lnN", 1.20)
+            #dc.add_systematic("doublemu", proc, "CMS_mcstat_qcd", "gmN {}".format(neff), alpha)
+            if nominal == 0.:
+                continue
 
         # PU
         pu_up = inputs.regions["doublemu"].processes[proc].systs["pu"].up / nominal
@@ -152,17 +186,35 @@ if __name__ == "__main__":
         dc.add_systematic("doublemu", proc, "CMS_puReweight", "lnN", 1+(abs(pu_up-1)+abs(1-pu_down))/2)
 
         # Muon SF
-        muon_up = inputs.regions["doublemu"].processes[proc].systs["muonSF"].up / nominal
-        muon_down = inputs.regions["doublemu"].processes[proc].systs["muonSF"].down / nominal
-        dc.add_systematic("doublemu", proc, "CMS_muonSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
+        muon_up = inputs.regions["doublemu"].processes[proc].systs["muonIsoSF"].up / nominal
+        muon_down = inputs.regions["doublemu"].processes[proc].systs["muonIsoSF"].down / nominal
+        dc.add_systematic("doublemu", proc, "CMS_muonIsoSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
+
+        muon_up = inputs.regions["doublemu"].processes[proc].systs["muonIdSF"].up / nominal
+        muon_down = inputs.regions["doublemu"].processes[proc].systs["muonIdSF"].down / nominal
+        dc.add_systematic("doublemu", proc, "CMS_muonIdSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
+
+        muon_up = inputs.regions["doublemu"].processes[proc].systs["muonTrackSF"].up / nominal
+        muon_down = inputs.regions["doublemu"].processes[proc].systs["muonTrackSF"].down / nominal
+        dc.add_systematic("doublemu", proc, "CMS_muonTrackSF", "lnN", 1+(abs(muon_up-1)+abs(1-muon_down))/2)
 
         # MET trigger
         met_up = inputs.regions["doublemu"].processes[proc].systs["metTrig2muSF"].up / nominal
         met_down = inputs.regions["doublemu"].processes[proc].systs["metTrig2muSF"].down / nominal
-        dc.add_systematic("doublemu", proc, "CMS_metTrig2muSF", "lnN", 1+(abs(met_up-1)+abs(1-met_down))/2)
+        dc.add_systematic("doublemu", proc, "CMS_metTrigMuSF", "lnN", 1+(abs(met_up-1)+abs(1-met_down))/2)
 
         # JECs
         jec_up = inputs.regions["doublemu"].processes[proc].systs["jec"].up / nominal
         jec_down = inputs.regions["doublemu"].processes[proc].systs["jec"].down / nominal
         dc.add_systematic("doublemu", proc, "CMS_jec", "lnN", 1+(abs(jec_up-1)+abs(1-jec_down))/2)
+
+    # rate params
+    dc.add_rateparam("r_mumu", "doublemu", "dymumu", 1)
+    dc.add_rateparam("r_nunu", "monojet", "znunu", "(@0*@1) r,r_mumu")
+
+    dc.sort_systematics([
+        "CMS_lumi", "CMS_puReweight", "CMS_jec", "CMS_metTrigSF",
+        "CMS_metTrigMuSF", "CMS_muonIdSF", "CMS_muonIsoSF", "CMS_muonTrackSF",
+        "CMS_qcd",
+    ])
     dc.write()
