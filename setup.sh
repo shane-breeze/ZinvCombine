40a23dc1f2162173c318b5a167a234264b99af38
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-06-04 20:54:55 +0100

    Add rootpy in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export cwd="$PWD"
 export CMSSW_BASE="/vols/build/cms/sdb15/ZinvWidth/HiggsCombine/CMSSW_8_1_0"
+export PYTHON_EXTRA="/home/hep/sdb15/externals"
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
@@ -14,4 +15,4 @@ eval `scramv1 runtime -sh`
 cd $cwd
 
 PATH=${PATH}:${cwd}/scripts
-PYTHONPATH=${PYTHONPATH}:${cwd}
+PYTHONPATH=${PYTHONPATH}:${cwd}:${PYTHON_EXTRA}/pip/lib/python2.7/site-packages
