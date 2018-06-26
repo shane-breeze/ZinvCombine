79106aa44733a3349c55a6c571c238dd9f79d1cb
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-06-26 11:55:07 +0100

    Add comment on expected fit

--- scripts/mlfit.sh
+++ scripts/mlfit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-combine ${1} -n Zinv -m 91 -M FitDiagnostics --forceRecreateNLL --saveNLL --plots --saveNormalizations --saveWithUncertainties --expectSignal 1
+combine ${1} -n Zinv -m 91 -M FitDiagnostics --forceRecreateNLL --saveNLL --plots --saveNormalizations --saveWithUncertainties --expectSignal 1 #-t -1
